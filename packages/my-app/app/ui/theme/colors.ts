10a8a85df78a839c65f69d46cf3c6ce657634288
sinandsean <sinandsean@gmail.com>
2024-04-12 15:34:14 +0900

    [app] chore: add image assets

---
+++ packages/my-app/app/ui/theme/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  black: "#000000",
+  gray: { 10: "#EEEEEE", 30: "#CACACA" },
+  "primary-light": { 10: "#60ABCB", 30: "#EEF9FD" },
+  primary: "#556981",
+  "steeblue-dark": { 10: "#556981" },
+};
